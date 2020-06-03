e88f0516fc20af56c63f2292ace9d2d9c71aa91c
Italox <italoaurelior@gmail.com>
2020-06-02 21:41:18 -0300

    :rotating_light: Adjust lint

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,11 +1,22 @@
 // import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm'
+import Transaction from '../models/Transaction'
 
-import Transaction from '../models/Transaction';
-
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    // await transactions = getCustomRepository(Transaction)
   }
 }
 
-export default CreateTransactionService;
+export default CreateTransactionService
