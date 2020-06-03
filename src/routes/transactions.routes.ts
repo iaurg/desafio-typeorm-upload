7fcec20f6165784f6683af8e42e29c5da5a445ee
Italox <italoaurelior@gmail.com>
2020-06-02 21:41:02 -0300

    :construction: Configuring routes

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService'
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
@@ -12,7 +12,10 @@ transactionsRouter.get('/', async (request, response) => {
 })
 
 transactionsRouter.post('/', async (request, response) => {
+  // Type pode ser income/outcome
   const { title, value, type, category } = request.body
+  const transaction = new CreateTransactionService()
+  transaction.execute({ title, value, type, category })
 })
 
 transactionsRouter.delete('/:id', async (request, response) => {
