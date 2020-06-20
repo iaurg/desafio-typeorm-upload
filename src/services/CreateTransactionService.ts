5091a58b83a178903fbe1cb1a807345a5865d844
Italox <italoaurelior@gmail.com>
2020-06-19 23:40:45 -0300

    :sparkles: New service for create transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,13 +1,16 @@
-// import AppError from '../errors/AppError';
-import { getCustomRepository } from 'typeorm'
+import { getCustomRepository, getRepository } from 'typeorm'
+import AppError from '../errors/AppError'
 import Transaction from '../models/Transaction'
+import Category from '../models/Category'
+import TransactionsRepository from '../repositories/TransactionsRepository'
 
 interface Request {
-  title: string;
-  value: number;
-  type: 'income' | 'outcome';
-  category: string;
+  title: string
+  value: number
+  type: 'income' | 'outcome'
+  category: string
 }
+
 class CreateTransactionService {
   public async execute({
     title,
@@ -15,7 +18,38 @@ class CreateTransactionService {
     type,
     category,
   }: Request): Promise<Transaction> {
-    // await transactions = getCustomRepository(Transaction)
+    const transactionsRepository = getCustomRepository(TransactionsRepository)
+    const categoriesRepository = getRepository(Category)
+
+    const { total } = await transactionsRepository.getBalance()
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance')
+    }
+
+    let transactionCategory = await categoriesRepository.findOne({
+      where: {
+        title: category,
+      },
+    })
+
+    if (!transactionCategory) {
+      transactionCategory = categoriesRepository.create({
+        title: category,
+      })
+
+      await categoriesRepository.save(transactionCategory)
+    }
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    })
+
+    await transactionsRepository.save(transaction)
+
+    return transaction
   }
 }
 
