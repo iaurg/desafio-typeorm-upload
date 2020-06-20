d8d2b58a65de15cface65e515afd33d9f8436f61
Italox <italoaurelior@gmail.com>
2020-06-19 23:39:33 -0300

    :sparkles: Create getBalance logic

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,33 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find()
+
+    const income = transactions.reduce(
+      (accumulator, transaction) =>
+        transaction.type === 'income'
+          ? accumulator + Number(transaction.value)
+          : accumulator,
+      0,
+    )
+
+    const outcome = transactions.reduce(
+      (accumulator, transaction) =>
+        transaction.type === 'outcome'
+          ? accumulator + Number(transaction.value)
+          : accumulator,
+      0,
+    )
+
+    const total = income - outcome
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    }
+
+    return balance
   }
 }
 
